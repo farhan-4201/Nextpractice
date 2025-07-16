68974f813fd6c937803dcfcc1834f8719dfc01ec
farhan4201 <farhantariq4636@gmail.com>
2025-07-16 17:17:38 +0500

    state and component as a function

--- app/page.js
+++ app/page.js
@@ -1,8 +1,36 @@
+"use client";
+import { useState } from "react";
 import "../app/globals.css";
-export default function home(){
-  return(
+export default function home() {
+  const [name, setName] = useState("Farhan");
+  const fruit = () => {
+    setName("tariq");
+  };
+  const Innercomp=()=>{
+    return(
+      <div>
+        <h1>inner component</h1>
+      </div>
+    )
+  }
+  return (
     <div>
-      <h1>Welcome to Our website</h1>
+      <h3>
+        <User name="farhan" />
+      </h3>
+      <User name="Daddy" />
+      <h1>Welcome to {name} website</h1>
+      <button onClick={() => fruit()}>click me</button>
+      <Innercomp/>
+      {Innercomp()}
     </div>
   );
 }
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>Hi this is {props.name}</h1>
+    </div>
+  );
+};
